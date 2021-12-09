cab72761592ac6ac68f48a50e274422ab96c0294
Gabit Nurkhanbekov <lwarayl@hotmail.com>
2021-12-09 21:38:32 +0600

    message

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -190,8 +190,8 @@ namespace dephBrowser
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(1392, 585);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.panel3);
